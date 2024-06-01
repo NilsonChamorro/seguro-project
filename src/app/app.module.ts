90ffcb593ab154c113f950143e48e69560e9974a
Nilson Chamorro <dany098260@gmail.com>
2024-05-31 21:50:10 -0400

    COMIT INICIAL
    EXAMEN

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,31 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { initializeAppCheck, ReCaptchaEnterpriseProvider, provideAppCheck } from '@angular/fire/app-check';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getPerformance, providePerformance } from '@angular/fire/performance';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { getRemoteConfig, provideRemoteConfig } from '@angular/fire/remote-config';
+import { getVertexAI, provideVertexAI } from '@angular/fire/vertexai-preview';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => 
+  initializeApp({"projectId":"tplpv-9c012","appId":"1:792142335017:web:d24e7d8f3c31082157a038",
+  "storageBucket":"tplpv-9c012.appspot.com","apiKey":"AIzaSyA-Q39VgTB18ARWkBaL1CK9i65H6C2yJrA",
+  "authDomain":"tplpv-9c012.firebaseapp.com","messagingSenderId":"792142335017",
+  "measurementId":"G-K1N0WEDDGP"})),
+   provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()), 
+   provideFunctions(() => getFunctions()), provideMessaging(() => getMessaging()), 
+   providePerformance(() => getPerformance()), provideStorage(() => getStorage()), 
+   provideRemoteConfig(() => getRemoteConfig())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
